77a273f008fc219da8dded0b42e9a9130743eb2d
fullstacktraning <fullstack.traning@gmail.com>
2026-04-25 17:41:12 +0530

    Enable caching in Spring Boot application

--- src/main/java/com/careerit/SpringbootCicdApplication.java
+++ src/main/java/com/careerit/SpringbootCicdApplication.java
@@ -2,8 +2,9 @@ package com.careerit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cache.annotation.EnableCaching;
 @SpringBootApplication
+@EnableCaching
 public class SpringbootCicdApplication {
 
 	public static void main(String[] args) {
