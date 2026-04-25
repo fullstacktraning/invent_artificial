4299912971fd58b37043d9ecf944aa1c9b561a06
fullstacktraning <fullstack.traning@gmail.com>
2026-04-25 17:45:06 +0530

    Update HomeController.java

--- src/main/java/com/careerit/controller/HomeController.java
+++ src/main/java/com/careerit/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.careerit.controller;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class HomeController {
 
+    @Cacheable("message")
     @GetMapping("/")
     public String home() {
+        System.out.println("Calling API...");
         return "Spring Boot CI/CD Success";
     }
 }
